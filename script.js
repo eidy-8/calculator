336aa513214092ff8f8977e7956824bfb4163f99
enrique <enriqueonaga@gmail.com>
2023-10-02 23:48:15 -0300

    update js logic

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let previousOperator = "";
 let currentOperator = "";
 let typedValue = [];
 let result = 0;
-//
+
 let operate = (numberA, operator, numberB) => {
     if (operator == "+") {
         return add(numberA, numberB)
@@ -28,23 +28,15 @@ function performOperation() {
 
     console.log(`typerValue: ${typedValue.length} inside the performOperation`);
     console.log(`numberA: ${numberA} inside the performOperation`);
+    console.log(`numberB: ${numberB} inside the performOperation`);
     console.log("------------------------------------------------");
 
     currentOperator = operator;
-    if (typedValue.length > 0) { //if any value was declared on typedValue then it means that it has to correctly calculate with the first operator and then input the next operator.
-        if (previousOperator != "") { //
+    if (typedValue.length > 0 && numberA != 0) { //if any value was declared on typedValue then it means that it has to correctly calculate with the first operator and then input the next operator.
+        if (previousOperator != "") { 
             operator = previousOperator;
         }
         numberB = typedValue.join(""); //it will concatenate all the number inputted by the user and it will turn into a string
-
-        if (operator == "*" && numberA == 0) { //as the numberB was already declared those conditions were made to make sure that numberA "neutralize" numberB. E.g. If I type (12*) the result would be (0*).
-            numberA = 1;
-        } else if (operator == "/" && numberA == 0) {
-            numberA = numberB * numberB;
-            console.log(numberA);
-        } else if (operator == "-" && numberA == 0) {
-            numberA = numberB * 2;
-        }
 
         display.innerHTML = "";
         console.log(numberA, numberB);
@@ -62,15 +54,21 @@ function performOperation() {
         numberB = 0;
         operator = currentOperator;
         typedValue = [];
-    } else { //if 
+    } else {
         if (numberA == 0) {
             numberA = typedValue.join("");
         }        
         display.innerHTML = "";
 
-        if (operator == "*" || operator == "/") {
-            numberB = 1;
-        } 
+        //"here's the problem"
+        if (operator == "*" && numberA == 0) { //as the numberB was already declared those conditions were made to make sure that numberA "neutralize" numberB. E.g. If I type (12*) the result would be (0*).
+            numberA = 1;
+        } else if (operator == "/" && numberA == 0) {
+            numberA = numberB * numberB;
+            console.log(numberA);
+        } else if (operator == "-" && numberA == 0) {
+            numberB = -numberB; 
+        }
 
         let result = operate(+numberA, operator, +numberB);
         display.innerHTML += result;
